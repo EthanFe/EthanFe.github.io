ee72d593382e11099d9dee593c1e634770612e3a
EthanFe <sneakcow@gmail.com>
2023-03-21 16:15:51 -0700

    initial

---
+++ precache-manifest.e78d47271187be5cb90039f971e0d68c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84323235f16dd5aa2ab28ea534c0b53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8988c94aae6c53400c7b",
+    "url": "/static/css/main.35eb68e4.chunk.css"
+  },
+  {
+    "revision": "c2e56a924fc38a6fe222",
+    "url": "/static/js/2.5f22498a.chunk.js"
+  },
+  {
+    "revision": "11d52683dba7ac59d186d337cbedb9d2",
+    "url": "/static/js/2.5f22498a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8988c94aae6c53400c7b",
+    "url": "/static/js/main.bac9f638.chunk.js"
+  },
+  {
+    "revision": "ed2290f452a674ec05a9",
+    "url": "/static/js/runtime-main.11c43f18.js"
+  }
+]);
